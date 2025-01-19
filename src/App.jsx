b24da996488b281eba79b32db6362cd27a22110f
Aman Kumar <amancollege04@gmail.com>
2025-01-19 17:57:12 +0530

    Wrap application in Router for improved routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import Footer from './components/Footer'
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/projects' element={<Projects />} />
-      </Routes>
-      <Footer />
-    </div>
-  )
+    <Router basename="/Portfolio">
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/projects' element={<Projects />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App
